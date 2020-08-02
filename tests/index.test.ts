bc14d8a78612bb47636ef5b7a1acce5549bb4a3d
Gao Sun <gaosun.dev@gmail.com>
2020-08-02 11:39:25 +0800

    add string and guard tests

---
+++ tests/index.test.ts
@@ -0,0 +1,74 @@
+import guard, { number, string, enumGuard, boolean, array, PowerGuardError } from '../src';
+import { expect } from 'chai';
+
+enum Foo {
+  key1 = 'key1',
+  key2 = 2,
+}
+
+const guardFunc = guard({
+  foo: number.gte(10).required,
+  bar: string.escaped.optional,
+  baz: enumGuard(Foo).optional.array,
+  bla: boolean.loose.optional,
+  bnd: array(guard({ foo: number.strict.required }), true),
+});
+
+describe('Power guard', () => {
+  it('should return proper object when needed', () => {
+    expect(
+      guardFunc({
+        foo: '10',
+        bar: '！ａｂｃ　ＡＢＣ！',
+        baz: [2, 'key1'],
+        bla: 1,
+        bnd: [{ foo: 123 }],
+      }),
+    ).deep.equals({
+      foo: 10,
+      bar: 'abc ABC',
+      baz: [Foo.key2, Foo.key1],
+      bla: true,
+      bnd: [{ foo: 123 }],
+    });
+
+    expect(guardFunc({ foo: 10, bla: '0' })).deep.equals({
+      foo: 10,
+      bar: undefined,
+      baz: undefined,
+      bla: false,
+      bnd: undefined,
+    });
+  });
+
+  it('should throw error when needed', () => {
+    expect(() =>
+      guardFunc({
+        foo: '10',
+        bar: '！ａｂｃ　ＡＢＣ！',
+        baz: [2, 'key1'],
+        bla: 1,
+        bnd: [{ foo: '123' }],
+      }),
+    ).throws(PowerGuardError);
+
+    expect(() =>
+      guardFunc({
+        foo: 'a10d',
+        bar: '！ａｂｃ　ＡＢＣ！',
+        baz: [2, 'key1'],
+        bla: 1,
+        bnd: [{ foo: 123 }],
+      }),
+    ).throws(PowerGuardError);
+
+    expect(() =>
+      guardFunc(`{
+        foo: 'a10d',
+        baz: [2, 'key1'],
+        bla: 1,
+        bnd: [{ foo: 123 }],
+      }`),
+    ).throws(PowerGuardError);
+  });
+});
---
+++ tests/string.test.ts
@@ -0,0 +1,51 @@
+import { string, PowerGuardError } from '../src';
+import { expect } from 'chai';
+
+describe('String guard', () => {
+  it('should return proper string when needed', () => {
+    expect(string.required('str')).equals('str');
+    expect(string.with(10).required('str')).equals('str');
+    expect(string.with(10).optional(undefined)).equals(undefined);
+    expect(string.escaped.optional('！ａｂｃ　ＡＢＣ！')).equals('abc ABC');
+    expect(string.escaped.optional('Image.PNG')).equals('ImagePNG');
+    expect(string.required('！ａｂｃ　ＡＢＣ！')).equals('！ａｂｃ　ＡＢＣ！');
+  });
+
+  it('should throw error when needed', () => {
+    expect(() => string.required(undefined)).throws(PowerGuardError);
+    expect(() => string.with(10).required('areallylongstring')).throws(PowerGuardError);
+    expect(() => string.escaped.optional(123)).throws(PowerGuardError);
+    expect(() => string.escaped.optional(true)).throws(PowerGuardError);
+    expect(() => string.escaped.optional({})).throws(PowerGuardError);
+    expect(() => string.escaped.required(undefined)).throws(PowerGuardError);
+  });
+
+  it('should return proper string array when needed', () => {
+    expect(string.required.array(['foo', 'bar', 'baz'])).deep.equals(['foo', 'bar', 'baz']);
+    expect(string.with(10).required.array(['str', '123', '1234567890'])).deep.equals([
+      'str',
+      '123',
+      '1234567890',
+    ]);
+    expect(string.with(10).optional.array(undefined)).deep.equals(undefined);
+    expect(
+      string.escaped.optional.array(['！ａｂｃ　ＡＢＣ！', '?ABC.abc', 'Image.PNG']),
+    ).deep.equals(['abc ABC', 'ABCabc', 'ImagePNG']);
+    expect(string.required.array(['！ａｂｃ　ＡＢＣ！', '?ABC.abc', 'Image.PNG'])).deep.equals([
+      '！ａｂｃ　ＡＢＣ！',
+      '?ABC.abc',
+      'Image.PNG',
+    ]);
+  });
+
+  it('should throw error for input array when needed', () => {
+    expect(() => string.required.array(undefined)).throws(PowerGuardError);
+    expect(() => string.with(10).required.array(['foo', 'areallylongstring'])).throws(
+      PowerGuardError,
+    );
+    expect(() => string.escaped.optional.array(['123', 123])).throws(PowerGuardError);
+    expect(() => string.escaped.optional.array([true])).throws(PowerGuardError);
+    expect(() => string.escaped.optional.array([{}])).throws(PowerGuardError);
+    expect(() => string.escaped.required.array(undefined)).throws(PowerGuardError);
+  });
+});
